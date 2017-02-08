0d3c8ba14ba5bb05ad90b69854846a7d05b65f77
Bill Kremer <bkrems12@gmail.com>
2017-02-08 11:51:26 -0600

    added registering route

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 router.post('/', function(req, res){
   // this is where we need to check the password
+
+
+
+
 });
 
 module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,56 @@
+var router = require('express').Router();
+var User = require('../models/user');
+
+router.post('/', function (req, res) {
+// add a new user
+
+  User.findOne({username: req.body.username }, function (err, user) {
+    if (err) {
+      console.log('error checking for existing user.');
+      res.sendStatus(500);
+      return;
+    };
+
+    if (user) {
+      console.log('username is taken');
+      return res.status(400).send('Username already taken');
+    }
+
+    var user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    user.save(function (err) {
+      if (err) {
+        console.log('error saving new user', err);
+        res.sendStatus(500);
+      } else {
+        console.log('created new user');
+        res.sendStatus(201);
+
+      }
+
+
+
+    })
+
+  })
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
